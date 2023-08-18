2be8c4bad1f82127ad1f18730c50596523e40736
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2023-08-18 07:14:13 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Routes from './routes';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style='light' />
+      <Routes />
+    </>
+  );
+}
---
+++ src/routes/index.jsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Login from '../screens/Login'
+import Register from '../screens/Register'
+
+const Stack = createNativeStackNavigator()
+
+function Routes() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Register" component={Register} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default Routes
