d8f226333a0f2c74d724990303a854e8c170c22c
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2025-05-21 07:47:57 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
 import Routes from './routes';
 
 export default function App() {
